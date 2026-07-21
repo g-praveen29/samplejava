58f6b1f50805516506671eac2121b1b485bf2deb
G.Praveen <guduripraveenkumar@gmail.com>
2026-07-21 15:31:24 +0530

    java program updated

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ class Hello{
 public static void main(String[] args){
 	String name = System.getenv("USERNAME_VAR");
 	if(name != null){
-		System.out.println("welcome to Devops Engineer" + " "+ name + "!");
+		System.out.println("Welcome to DevOps Engineer" + " "+ name + "!");
+		System.out.println("Have a Nice Day!");
 	}
 	else{
 		System.out.println("Environment variable not set!");
